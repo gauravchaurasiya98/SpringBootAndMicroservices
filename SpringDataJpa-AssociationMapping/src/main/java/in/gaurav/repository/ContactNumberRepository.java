2b08f002ed601732210e224177de9a61b0815655
Gaurav Chaurasiya <gaurav.chaurasiya@yahoo.com>
2022-08-24 11:29:09 +0530

    Delete SpringDataJpa-AssociationMapping directory

--- SpringDataJpa-AssociationMapping/src/main/java/in/gaurav/repository/ContactNumberRepository.java
+++
@@ -1,9 +0,0 @@
-package in.gaurav.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import in.gaurav.entity.ContactNumber;
-
-public interface ContactNumberRepository extends JpaRepository<ContactNumber, Long> {
-
-}
