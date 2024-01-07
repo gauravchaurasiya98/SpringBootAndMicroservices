9ccc1ad2f676a69e7e3839cc2f231d086b3205ab
Gaurav Chaurasiya <gaurav.chaurasiya@yahoo.com>
2024-01-08 01:50:41 +0530

    Delete SpringDataJpa directory

--- SpringDataJpa/src/main/java/in/gaurav/repo/StudentRepo.java
+++
@@ -1,9 +0,0 @@
-package in.gaurav.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import in.gaurav.entity.Student;
-
-public interface StudentRepo extends JpaRepository<Student, Integer> {
-
-}
