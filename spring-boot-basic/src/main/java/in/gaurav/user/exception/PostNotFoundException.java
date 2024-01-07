e12068463a226b0d37f15060b680425e0159ad05
Gaurav Chaurasiya <gaurav.chaurasiya@yahoo.com>
2024-01-08 01:40:57 +0530

    Delete spring-boot-basic directory

--- spring-boot-basic/src/main/java/in/gaurav/user/exception/PostNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package in.gaurav.user.exception;
-
-// @ResponseStatus(HttpStatus.NOT_FOUND)
-public class PostNotFoundException extends RuntimeException {
-    public PostNotFoundException(String message) {
-        super(message);
-    }
-}
